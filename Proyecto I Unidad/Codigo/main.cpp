404de82048ada4dba7d8d361d16bfc84ffab92ed
Keilly Giron <gironkeilly@gmail.com>
2021-06-21 16:24:26 -0600

    editando

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -3,10 +3,32 @@
 #include "snake.h"
 
 using namespace std;
+void starShip();
+void snake();
+
 
 int main(int argc, char const *argv[])
+
 {
-    system("cls");
+    int juego;
+    cout<<"**************"<<endl;
+    cout<<"MENU DE JUEGOS"<<endl;
+    cout<<"**************"<<endl; 
+     cout<<  "Seleccione un juego"<<endl;
+    cout<<"1 - Starship"<<endl;
+    cout<<"2 - Snake"<<endl;
+   cout<<"Ingrese un numero del menu para seleccionar un juego:"<<endl;
+    cin>>juego;
+    switch(juego)
+    {
+        case 1:
+             starShip();
+             break;
+        case 2:
+            snake();
+            break;
+    }
+    system("pause");
 
     // starShip();
     // snake();
