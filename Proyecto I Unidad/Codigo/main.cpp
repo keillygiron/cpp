2c465483afa8ab19d30f7672f9af437365c44639
Keilly Giron <gironkeilly@gmail.com>
2021-06-21 17:04:21 -0600

    proyecto

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -10,15 +10,15 @@ int main(int argc, char const *argv[])
     int juego = 0;
     system("cls");
    
-    cout<<"******"<<endl;
-    cout<<"* MENU DE JUEGOS *"<<endl;
-    cout<<"******"<<endl;
-    cout<<endl;
-    cout<<"Seleccione un juego"<<endl;
-    cout<<"1 - StarShip"<<endl;
+    cout<<"**************"<<endl;
+    cout<<"MENU DE JUEGOS"<<endl;
+    cout<<"**************"<<endl; 
+     cout<<  "Seleccione un juego"<<endl;
+    cout<<"1 - Starship"<<endl;
     cout<<"2 - Snake"<<endl;
-    cout<<"Ingrese un numero para jugar: "<<endl;
-    cin>> juego;
+   
+   cout<<"Ingrese un numero del menu para seleccionar un juego:"<<endl;
+    cin>>juego;
 
     switch (juego)
     {
