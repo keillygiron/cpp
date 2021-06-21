070dba028ebc49c52a40d5a984e9eb6bd6ff9717
Keilly Giron <gironkeilly@gmail.com>
2021-06-21 14:37:26 -0600

    editando

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -4,12 +4,45 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+main()
 {
-    system("cls");
+    int juego;
 
-    // starShip();
-    // snake();
+    system("cls");
+    while(juego !=2) 
+   {
+    cout<<"**************"<<endl;
+    cout<<"MENU DE JUEGOS"<<endl;
+    cout<<"**************"<<endl; 
+     cout<<  "Seleccione un juego"<<endl;
+    cout<<"1 - Starship"<<endl;
+    cout<<"2 - Snake"<<endl;
+   cout<<"Ingrese un numero del menu para seleccionar un juego:"<<endl;
+    cin>>juego;
+}
+  
+switch (juego)
+{
+case 1:
+   system("cls");
+   cout<<"StarShip"<<endl;
+   system("pause");
+ break;
+ case 2:
+   system("cls");
+   cout<<"Snake"<<endl;
+   system("pause");
+
+default:
+   if(juego <1 || juego >2 )
+   {
+       cout<<"Este numero no es valido..."<<endl;
+   
+    }
+    break;
+}  
+  starShip();
+    snake();
 
     return 0;
 }
